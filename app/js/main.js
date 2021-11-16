f5c8d4e49b0083722fa48e482d8ec1ddfa13f1b0
vladk7072 <vladk7072@gmail.com>
2021-11-16 21:56:58 +0200

    16.11.2021-3

--- app/js/main.js
+++ app/js/main.js
@@ -8,22 +8,20 @@ $(function () {
       crossFade: true,
     },
     navigation: {
-      nextEl: '.trip__btn-next',
-      prevEl: '.trip__btn-prev',
+      nextEl: ".trip__btn-next",
+      prevEl: ".trip__btn-prev",
     },
-  })
+  });
 
   let slidersTotal = document.querySelector(".trip__slide-num");
-  tripSlider.on("slideChange", function(){
+  tripSlider.on("slideChange", function () {
     let currentSlide = ++tripSlider.realIndex;
     slidersTotal.innerHTML = "0" + currentSlide + ".";
-  })
+  });
 
   const tripBar = document.querySelector(".trip__bar");
   tripBar.addEventListener("click", (e) => {
+    $(".trip__bar-item--active").removeClass("trip__bar-item--active");
     e.target.classList.add("trip__bar-item--active");
-  })
-
-  
-
+  });
 });
