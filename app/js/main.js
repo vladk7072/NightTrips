90f025f05ee7e5b6bd42117ac826f2bac889a754
vladk7072 <vladk7072@gmail.com>
2021-11-16 18:09:02 +0200

    16.11.2021

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+  const tripSlider = new Swiper(".trip__slider", {
+    hashNavigation: {
+      watchState: true,
+    },
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    navigation: {
+      nextEl: '.trip__btn-next',
+      prevEl: '.trip__btn-prev',
+    },
+  })
+
+  let slidersTotal = document.querySelector(".trip__slide-num");
+  tripSlider.on("slideChange", function(){
+    let currentSlide = ++tripSlider.realIndex;
+    slidersTotal.innerHTML = "0" + currentSlide + ".";
+  })
+
+  const tripBar = document.querySelector(".trip__bar");
+  tripBar.addEventListener("click", (e) => {
+    e.target.classList.add("trip__bar-item--active");
+  })
+
+});
